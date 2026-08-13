2628f64ddb4f47a0ee8bfeb427ecfa5162814740
Michael Zelensky <michaelzelensky@gmail.com>
2026-08-13 10:33:02 +0700

    Switch to vite pwa plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,59 @@
 ﻿import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/budgetclick-pwa/",
 
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "BudgetClick",
+        short_name: "BudgetClick",
+        start_url: "/budgetclick-pwa/",
+        scope: "/budgetclick-pwa/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0f6e56",
+
+        icons: [
+          {
+            src: "icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512-maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+
+      workbox: {
+        navigateFallback: "/budgetclick-pwa/index.html",
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+
   server: {
     fs: {
       allow: [".."],
